8169190e878f957966e0dd03c5341070f47a20eb
Aevum Sonne <aevumsonne@gmail.com>
2020-12-01 18:57:40 +0300

    Add cases for states

--- 8/GumballMachine/GumballMachineTests/GumballMachineTests.swift
+++ 8/GumballMachine/GumballMachineTests/GumballMachineTests.swift
@@ -15,102 +15,175 @@ import TextStream
 
 class GumballMachineTests: XCTestCase {
   
-  // MARK: SoldOutState tests
+  override class func setUp() {
+    outStream = TextStream.OStream(type: .logs)
+  }
   
-  func testCorrectSoldOutStateWhenNumBallsEqualsToZero() {
-    let expected = "Mighty Gumball, Inc.\nSwift-enabled Standing Gumball Model #2020 (with state)\nInventory: 0 gumballs\nMachine is sold out"
+  fileprivate static var outStream = TextStream.OStream(type: .logs)
+
+  
+  // MARK: NoQuarterState tests
+  
+  func testCorrectNoQuarterStateNoChangeIfInsertQuarterNotCalled() {
+    let expected = "Mighty Gumball, Inc.\nSwift-enabled Standing Gumball Model #2020 (with state)\nInventory: 5 gumballs\nMachine is waiting for quarter"
     
-    let outStream = TextStream.OStream(type: .logs)
-    let gumballMachine = GumballMachine(numBalls: 0, outStream: outStream)
-    
+    let gumballMachine = GumballMachine(numBalls: 5, outStream: GumballMachineTests.outStream)
+        
+    gumballMachine.ejectQuarter()
+    gumballMachine.turnCrank()
+        
     XCTAssertEqual(expected, gumballMachine.toString())
   }
   
-  func testRecieveMessageSoldOutIfMachineIsEmptyOnQuarterInserting() {
-    let expected = "You can\'t insert a quarter, the machine is sold out\n"
+  func testCorrectNoQuarterStateInsertAndInjectQuarter() {
+    let expected = "Mighty Gumball, Inc.\nSwift-enabled Standing Gumball Model #2020 (with state)\nInventory: 5 gumballs\nMachine is waiting for quarter"
     
-    let outStream = TextStream.OStream(type: .logs)
-    let gumballMachine = GumballMachine(numBalls: 0, outStream: outStream)
-    
-    gumballMachine.insertQuarter()
-    
-    XCTAssertEqual(expected, outStream.logs)
-  }
-  
-  func testBuyAllGumsAndRecieveMessageSoldOut() {
-    let expected = "You can\'t insert a quarter, the machine is sold out"
-    
-    let outStream = TextStream.OStream(type: .logs)
-    let gumballMachine = GumballMachine(numBalls: 5, outStream: outStream)
-    
-    GumballMachineActions.buyGum(gumballMachine: gumballMachine)
-    GumballMachineActions.buyGum(gumballMachine: gumballMachine)
-    GumballMachineActions.buyGum(gumballMachine: gumballMachine)
-    GumballMachineActions.buyGum(gumballMachine: gumballMachine)
-    GumballMachineActions.buyGum(gumballMachine: gumballMachine)
+    let gumballMachine = GumballMachine(numBalls: 5, outStream: GumballMachineTests.outStream)
     
     gumballMachine.insertQuarter()
-    
-    XCTAssertEqual(expected, outStream.lastLog)
-  }
-  
-  func testCorrectSoldOutStateAfterEjecting() {
-    let expected = "Mighty Gumball, Inc.\nSwift-enabled Standing Gumball Model #2020 (with state)\nInventory: 0 gumballs\nMachine is sold out"
-    
-    let outStream = TextStream.OStream(type: .logs)
-    let gumballMachine = GumballMachine(numBalls: 0, outStream: outStream)
-    
     gumballMachine.ejectQuarter()
     
     XCTAssertEqual(expected, gumballMachine.toString())
   }
   
-  func testCorrectSoldOutStateAfterInserting() {
-    let expected = "Mighty Gumball, Inc.\nSwift-enabled Standing Gumball Model #2020 (with state)\nInventory: 0 gumballs\nMachine is sold out"
+  func testCorrectNoQuarterStateChangeIfInsertQuarterCalled() {
+    let expected = "Mighty Gumball, Inc.\nSwift-enabled Standing Gumball Model #2020 (with state)\nInventory: 5 gumballs\nMachine is waiting for turn of crank"
     
-    let outStream = TextStream.OStream(type: .logs)
-    let gumballMachine = GumballMachine(numBalls: 0, outStream: outStream)
+    let gumballMachine = GumballMachine(numBalls: 5, outStream: GumballMachineTests.outStream)
     
+    gumballMachine.ejectQuarter()
+    gumballMachine.turnCrank()
+    gumballMachine.insertQuarter()
+        
+    XCTAssertEqual(expected, gumballMachine.toString())
+  }
+  
+  
+  // MARK: HasQuarterState
+  
+  func testCorrectSetHasQuarterStateWhenInsertQuarter() {
+    let expected = "Mighty Gumball, Inc.\nSwift-enabled Standing Gumball Model #2020 (with state)\nInventory: 5 gumballs\nMachine is waiting for turn of crank"
+    
+    let gumballMachine = GumballMachine(numBalls: 5, outStream: GumballMachineTests.outStream)
+        
+    gumballMachine.insertQuarter()
+        
+    XCTAssertEqual(expected, gumballMachine.toString())
+  }
+  
+  func testCorrectHasQuarterStateNoChangeIfInsertAnotherQuarter() {
+    let expected = "Mighty Gumball, Inc.\nSwift-enabled Standing Gumball Model #2020 (with state)\nInventory: 5 gumballs\nMachine is waiting for turn of crank"
+    
+    let gumballMachine = GumballMachine(numBalls: 5, outStream: GumballMachineTests.outStream)
+        
+    gumballMachine.insertQuarter()
     gumballMachine.insertQuarter()
     
     XCTAssertEqual(expected, gumballMachine.toString())
+    
+    let expectedMessage = "You can\'t insert another quarter"
+    
+    XCTAssertEqual(expectedMessage, GumballMachineTests.outStream.lastLog)
   }
   
-  func testCorrectSoldOutStateAfterTurningCrank() {
-    let expected = "Mighty Gumball, Inc.\nSwift-enabled Standing Gumball Model #2020 (with state)\nInventory: 0 gumballs\nMachine is sold out"
+  func testCorrectChangingHasQuarterStateWhenInsertQuarter() {
+    let expected = "Mighty Gumball, Inc.\nSwift-enabled Standing Gumball Model #2020 (with state)\nInventory: 5 gumballs\nMachine is waiting for turn of crank"
     
-    let outStream = TextStream.OStream(type: .logs)
-    let gumballMachine = GumballMachine(numBalls: 0, outStream: outStream)
-    
+    let gumballMachine = GumballMachine(numBalls: 5, outStream: GumballMachineTests.outStream)
+        
+    gumballMachine.insertQuarter()
     gumballMachine.turnCrank()
     
-    XCTAssertEqual(expected, gumballMachine.toString())
+    XCTAssertNotEqual(expected, gumballMachine.toString())
   }
   
-  
-  // MARK: SoldState test
+  // MARK: SoldState tests
   
   func testCorrectSoldStateBuyGum() {
     let expected = "A gumball comes rolling out the slot..."
     
-    let outStream = TextStream.OStream(type: .logs)
-    let gumballMachine = GumballMachine(numBalls: 2, outStream: outStream)
+    let gumballMachine = GumballMachine(numBalls: 2, outStream: GumballMachineTests.outStream)
 
     GumballMachineActions.buyGum(gumballMachine: gumballMachine)
     
-    XCTAssertEqual(expected, outStream.lastLog)
+    XCTAssertEqual(expected, GumballMachineTests.outStream.lastLog)
   }
   
   func testCorrectSoldStateChangingAfterTurnCrack() {
     let expected = "You need to pay first"
     
-    let outStream = TextStream.OStream(type: .logs)
-    let gumballMachine = GumballMachine(numBalls: 2, outStream: outStream)
+    let gumballMachine = GumballMachine(numBalls: 2, outStream: GumballMachineTests.outStream)
 
     GumballMachineActions.buyGum(gumballMachine: gumballMachine)
     gumballMachine.turnCrank()
     
-    XCTAssertEqual(expected, outStream.lastLog)
+    XCTAssertEqual(expected, GumballMachineTests.outStream.lastLog)
   }
+}
 
+
+// MARK: SoldOutState tests
+
+func testCorrectSoldOutStateWhenNumBallsEqualsToZero() {
+  let expected = "Mighty Gumball, Inc.\nSwift-enabled Standing Gumball Model #2020 (with state)\nInventory: 0 gumballs\nMachine is sold out"
+  
+  let gumballMachine = GumballMachine(numBalls: 0, outStream: GumballMachineTests.outStream)
+  
+  XCTAssertEqual(expected, gumballMachine.toString())
+}
+
+func testRecieveMessageSoldOutIfMachineIsEmptyOnQuarterInserting() {
+  let expected = "You can\'t insert a quarter, the machine is sold out\n"
+  
+  let gumballMachine = GumballMachine(numBalls: 0, outStream: GumballMachineTests.outStream)
+  
+  gumballMachine.insertQuarter()
+  
+  XCTAssertEqual(expected, GumballMachineTests.outStream.logs)
+}
+
+func testBuyAllGumsAndRecieveMessageSoldOut() {
+  let expected = "You can\'t insert a quarter, the machine is sold out"
+  
+  let gumballMachine = GumballMachine(numBalls: 5, outStream: GumballMachineTests.outStream)
+  
+  GumballMachineActions.buyGum(gumballMachine: gumballMachine)
+  GumballMachineActions.buyGum(gumballMachine: gumballMachine)
+  GumballMachineActions.buyGum(gumballMachine: gumballMachine)
+  GumballMachineActions.buyGum(gumballMachine: gumballMachine)
+  GumballMachineActions.buyGum(gumballMachine: gumballMachine)
+  
+  gumballMachine.insertQuarter()
+  
+  XCTAssertEqual(expected, GumballMachineTests.outStream.lastLog)
+}
+
+func testCorrectSoldOutStateAfterEjecting() {
+  let expected = "Mighty Gumball, Inc.\nSwift-enabled Standing Gumball Model #2020 (with state)\nInventory: 0 gumballs\nMachine is sold out"
+  
+  let gumballMachine = GumballMachine(numBalls: 0, outStream: GumballMachineTests.outStream)
+  
+  gumballMachine.ejectQuarter()
+  
+  XCTAssertEqual(expected, gumballMachine.toString())
+}
+
+func testCorrectSoldOutStateAfterInserting() {
+  let expected = "Mighty Gumball, Inc.\nSwift-enabled Standing Gumball Model #2020 (with state)\nInventory: 0 gumballs\nMachine is sold out"
+  
+  let gumballMachine = GumballMachine(numBalls: 0, outStream: GumballMachineTests.outStream)
+  
+  gumballMachine.insertQuarter()
+  
+  XCTAssertEqual(expected, gumballMachine.toString())
+}
+
+func testCorrectSoldOutStateAfterTurningCrank() {
+  let expected = "Mighty Gumball, Inc.\nSwift-enabled Standing Gumball Model #2020 (with state)\nInventory: 0 gumballs\nMachine is sold out"
+  
+  let gumballMachine = GumballMachine(numBalls: 0, outStream: GumballMachineTests.outStream)
+  
+  gumballMachine.turnCrank()
+  
+  XCTAssertEqual(expected, gumballMachine.toString())
 }
